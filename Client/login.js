d0ab53e907f95eb30fead50a3c0136b5b736ad7f
EC2 Default User <ec2-user@ip-172-31-12-70.eu-west-2.compute.internal>
2022-06-19 17:02:37 +0000

    Client added

---
+++ Client/login.js
@@ -0,0 +1,44 @@
+const http = require('http');
+
+const options = {
+  host: '18.134.160.183',
+  port:3000,
+  path: '/login',
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json; charset=UTF-8'
+  }
+};
+
+const req = http.request(options, (res) => {
+    if (res.statusCode !== 201) {
+      console.error(`Did not get a Created from the server. Code: ${res.statusCode}`);
+      res.resume();
+      return;
+    }
+  
+    let data = '';
+  
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+  
+    res.on('close', () => {
+      console.log('Added new user');
+      console.log(JSON.parse(data));
+    });
+  });
+
+const requestData = {
+    user: 'user1',
+    password: 'pass123'
+  };
+  
+req.write(JSON.stringify(requestData));
+
+req.end();
+
+req.on('error', (err) => {
+  console.error(`Encountered an error trying to make a request: ${err.message}`);
+});
