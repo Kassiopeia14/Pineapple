f6988930efe1565738480675ab74e6ba09e0f5a3
EC2 Default User <ec2-user@ip-172-31-12-70.eu-west-2.compute.internal>
2022-06-19 17:20:12 +0000

    Server works with node http Client

--- Server/routes/login.js
+++ Server/routes/login.js
@@ -5,7 +5,9 @@ router.post('/', function(req, res, next) {
 	var user_name = req.body.user;
 	var password = req.body.password;
 	console.log('User name = ' + user_name + ', password is ' + password);
-	res.end('yes');
+	res.json({
+		text: 'Login is succesfull'
+	});
 });
 
 module.exports = router;
