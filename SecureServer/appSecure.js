c397bb51a395c98a6bc3fa581770b59d2e1d3206
EC2 Default User <ec2-user@ip-172-31-12-70.eu-west-2.compute.internal>
2022-06-26 13:30:14 +0000

    certificate and SecureServer added

---
+++ SecureServer/appSecure.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslCertification/server.key', 'utf8');
+var certificate = fs.readFileSync('sslCertification/server.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
+
+// your express configuration here
+
+app.get('/', function(req, res, next) {
+    res.json({
+        data : 'buba'
+    });
+  });
+
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+let PORT1 = 8080;
+let PORT2 = 3000;
+
+httpServer.listen(PORT1,()=>{
+    console.log('HTTP on port:', PORT1);
+    });
+httpsServer.listen(PORT2,()=>{
+    console.log('HTTPS on port:', PORT2);
+    });
